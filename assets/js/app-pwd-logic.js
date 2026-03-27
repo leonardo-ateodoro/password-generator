0c8efef88c3cbb202b8cad9a1f90d5e0b0d9cb08
leonardo <leonardo.a.teodoro@aluno.senai.br>
2026-03-27 09:34:47 -0300

    JS

--- assets/js/app-pwd-logic.js
+++ assets/js/app-pwd-logic.js
@@ -4,4 +4,9 @@ const clearButton = document.querySelector('.app-pwd__button--clear');
 const sizePassword = document.querySelector('.app-pwd__size');
 const password = document.querySelector('.app-pwd__output'); 
 const containerPassword = document.querySelector('.app-pwd__result');
-const welcomeElement = document.querySelector()
+const welcomeElement = document.querySelector('.app-pwd__welcome');
+const datetimeElement = document.querySelector ('.app-pwd__datetime');
+
+const charsets = {
+    uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+}
