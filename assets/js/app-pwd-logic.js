595bbd2b84ca79c89c5078cccf5e9c88f8ab6f2a
leonardo <leonardo.a.teodoro@aluno.senai.br>
2026-03-27 08:48:24 -0300

    fazendo o app.js

---
+++ assets/js/app-pwd-logic.js
@@ -0,0 +1,7 @@
+const sliderElement = document.querySelector('.app.pwd__slider'); 
+const buttonElement = document.querySelector('app-pwd__button');
+const clearButton = document.querySelector('.app-pwd__button--clear');
+const sizePassword = document.querySelector('.app-pwd__size');
+const password = document.querySelector('.app-pwd__output'); 
+const containerPassword = document.querySelector('.app-pwd__result');
+const welcomeElement = document.querySelector()
