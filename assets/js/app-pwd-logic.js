4ae3d3c14caeae85154e5d8428aebb0c8741555f
leonardo <leonardo.a.teodoro@aluno.senai.br>
2026-04-02 11:05:50 -0300

    js

--- assets/js/app-pwd-logic.js
+++ assets/js/app-pwd-logic.js
@@ -47,3 +47,77 @@ const formatarDataHora =() => {
 
     return `${diaSemana}, ${dia}/${mes}/${ano} ${hora}:${minuto}:${segundo}`;
 };
+
+
+const atualizarHeader = () => {
+    welcomeElement.textContent = `${getSaudacao}!`;
+    datetimeElement.textContent = formatarDataHora();
+};
+
+setInterval(atualizarHeader, 1000);
+atualizarHeader();
+
+sizePassword.textContent = sliderElement.value;
+
+sliderElement.addEventListener('input', (e) => {
+    sizePassword.textContent = e.target.value;
+});
+
+const generatePassword = () => {
+    let selectedCharset = '';
+
+    const uppercaseChecked = document.querySelector('.uppercase-check').checked;
+    const lowercaseChecked = document.querySelector('.lowercase-check').checked;
+    const cnumbersChecked = document.querySelector ('.numbers-check').checked;
+    const specialChecked = document.querySelector('.special-check').checked;
+
+    if (uppercaseChecked) selectedCharset += charsets.uppercase;
+    if (lowercaseChecked) selectedCharset += charsets.lowercase;
+    if (cnumbersChecked) selectedCharset += charsets.numbers;
+    if (specialChecked) selectedCharset += charsets.special;
+    
+    if (!selectedCharset) {
+        selectedCharset = Object.values (charsets).join('');
+        document.querySelector ('.uppercase-check').checked = true;
+        document.querySelector('.lowercase-check').checked = true;
+        document.querySelector('.numbweas-check'). checked = true;
+        document.querySelector('.special-check').checked = true;
+    }
+
+    let pass = '';
+
+    for (let i = 0; i < sliderElement.value; ++i) {
+     pass += selectedCharset.charAt(Math.floor(Math.random() * selectedCharset.length));   
+    }
+
+    containerPassword.classList.remove('hide');
+
+    password.textContent = pass;
+
+    novaSenha = pass;
+
+    historicoSenhas.unshift(pass);
+
+    if (historicoSenhas.length > 3) {
+        historicoSenhas.pop();
+    }
+
+    const historico = document.querySelector('.app-pwd__history');
+    if (historico) {
+        historico.style.display = 'block';
+
+        historico.querySelector('.app-pwd__history-list').innerHTML = historicoSenhas
+            .map(senha => `<li class="app-pwd__history-item">${senha}</li>`)
+            .join('');
+    }
+};
+
+const copyPassword = () => {
+    alert ('Senha copiada com sucesso!'); 
+    navigator.clipboard.writeText(novaSenha);
+};
+
+buttonElement.addEventListener('click', g)
+
+
+
